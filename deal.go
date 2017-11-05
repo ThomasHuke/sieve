1075a9d48ef76bd572f05098cb03f737ec0e7d22
ThomasHuke <thomashuke@icloud.com>
2017-11-05 16:23:41 +0800

    change

--- deal.go
+++ deal.go
@@ -14,8 +14,23 @@ type Number0 struct {
 	src string
 }
 
+//
 func (n0 Number0) DealWith(src string) (string, error) {
 	_, err := regexp.MatchString(Pattern["number0"], src)
+	if err == nil {
+		return src, err
+	} else {
+		return "error", err
+	}
+}
+
+type Number1 struct {
+	src string
+}
+
+//
+func (n1 Number1) DealWith(src string) (string, error) {
+	_, err := regexp.MatchString(Pattern["number1"], src)
 	if err == nil {
 		return src, err
 	} else {
--- rule.go
+++ rule.go
@@ -1,6 +1,7 @@
-//Package Sieve- rule文件就是这个包中的规则制定包。
+//Package sieve rule文件就是这个包中的规则制定包。
 package sieve
 
+//Pattern 代表了这个正则表达式的具体内容
 var Pattern = map[string]string{
 	"number0": "^\\w+@\\w+\\.\\w+$",
 	"number1": "^\\w+$",
