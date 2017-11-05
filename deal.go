20e7415fb4c6f4b2a9e8613c94ea8b328d9fd973
ThomasHuke <thomashuke@icloud.com>
2017-11-05 15:51:28 +0800

    add

--- deal.go
+++ deal.go
@@ -1,6 +1,24 @@
 //Package sieve主要是处理
 package sieve
 
+import (
+	"regexp"
+)
+
 type Sie interface {
 	DealWith(src string) (string, error)
+}
+
+//number0
+type Number0 struct {
+	src string
+}
+
+func (n0 Number0) DealWith(src string) (string, error) {
+	_, err := regexp.MatchString(Pattern["number0"], src)
+	if err == nil {
+		return src, err
+	} else {
+		return "error", err
+	}
 }
---
+++ rule.go
@@ -0,0 +1,7 @@
+//Package Sieve- rule文件就是这个包中的规则制定包。
+package sieve
+
+var Pattern = map[string]string{
+	"number0": "",
+	"number1": "",
+}
